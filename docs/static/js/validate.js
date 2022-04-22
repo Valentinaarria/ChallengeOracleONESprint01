cc1a51bad56fbab604c4dcff7d3bd9f87378d572
Valentina Arria <valentinaarria@gmail.com>
2022-04-22 15:32:30 -0300

    button disabled on validation failed

--- docs/static/js/validate.js
+++ docs/static/js/validate.js
@@ -20,16 +20,25 @@ const formValidate = (e) => {
 
 const fieldValidate =(expression, input) => {	
     let warningText= document.getElementById('warning'); 
-	let warningImg=document.getElementById('warning-icon')
+	let warningImg=document.getElementById('warning-icon');
+	let bttEn=document.getElementById('btt-Enc');
+	let bttDes=document.getElementById('btt-Des');
 	
 	if((expression.test(input.value)) || (input.value == "")){
-		warningText.style.color="var(--gray-400)"
-		warningImg.style.fill="var(--gray-400)"		
+		warningText.style.color="var(--gray-400)";
+		warningImg.style.fill="var(--gray-400)"	;
+		bttEn.disabled=false;
+		bttDes.disabled=false;
+
 	}
 	
 	else {
      warningText.style.color= "red";
-	 warningImg.style.fill="red"		 
+	 warningImg.style.fill="red"	
+	 bttEn.disabled=true;
+		bttDes.disabled=true;
+		
+		 
 	}
 };
 
